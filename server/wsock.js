16ca0af7b35ef764cd763ecc062f2a51dad22550
James Halliday <mail@substack.net>
2015-06-10 18:53:34 -0700

    onend destroy necessary for some reason

--- server/wsock.js
+++ server/wsock.js
@@ -1,14 +1,12 @@
-var onend = require('end-of-stream');
 var http = require('http');
 var wsock = require('websocket-stream');
+var onend = require('end-of-stream');
 
 module.exports = function (pn) {
     var server = http.createServer(function (req, res) { res.end() });
     wsock.createServer({ server: server }, function (stream) {
-        var addr = stream.socket.upgradeReq.socket.remoteAddress;
-        var port = stream.socket.upgradeReq.socket.remotePort;
-        pn._connections[addr + ':' + port] = stream;
         stream.pipe(pn.createStream()).pipe(stream);
+        onend(stream, function () { stream.destroy() });
     });
     return server;
 };
